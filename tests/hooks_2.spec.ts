d10c06bafde9c3873f501d9876f916945ee1e784
Bishal Thapaliya <bishalthapaliya@Bishals-MacBook-Pro.local>
2026-08-24 21:46:39 +0200

    add tests for inventory validation and product addition using Playwright hooks

---
+++ tests/hooks_2.spec.ts
@@ -0,0 +1,37 @@
+import { test, expect, Page } from '@playwright/test';
+
+// The goal of this file is to show how to user beforAll() and afterAll() hooks
+
+let page:Page;
+
+test.beforeAll(async ({ browser }) => {
+    page = await browser.newPage();
+    await page.goto('https://saucedemo.com');
+
+    // login
+    await page.getByPlaceholder('Username').fill('standard_user');
+    await page.getByPlaceholder('Password').fill('secret_sauce');
+    await page.getByRole('button', { name: 'Login'}).click();
+});
+
+test.afterAll(async () => {
+    // logout
+    await page.getByRole('button', { name: 'Open Menu'}).click();
+    await page.getByRole('link', { name: 'Logout'}).click();
+    await expect(page).toHaveURL(/www.saucedemo.com/);
+    await expect(page).not.toHaveURL(/inventory/);
+});
+
+test('should validate inventory correctly', async () => {
+    // validate successful login by checking current URL
+    await expect(page).toHaveURL(/inventory/);
+
+    await page.waitForTimeout(5000);
+});
+
+test('should add product to cart correctly', async () => {
+    await page.locator('#add-to-cart-sauce-labs-backpack').click();
+    expect(await page.locator('.shopping_cart_badge')).toBeVisible();
+
+    await page.waitForTimeout(5000);
+});
