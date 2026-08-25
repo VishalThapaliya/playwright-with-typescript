e1a591b2207e36f04107b71c7de1889b2c8be838
Bishal Thapaliya <bishalthapaliya@Bishals-MacBook-Pro.local>
2026-08-25 16:32:20 +0200

    fix: correct import case for LoginPage in pomLogin.spec.ts

--- tests/pomLogin.spec.ts
+++ tests/pomLogin.spec.ts
@@ -1,5 +1,5 @@
 import test, { expect } from "@playwright/test";
-import { LoginPage } from "../pages/loginPage";
+import { LoginPage } from "../pages/LoginPage";
 import { ProductListPage } from "../pages/ProductListPage";
 import { CartPage } from "../pages/CartPage";
 
