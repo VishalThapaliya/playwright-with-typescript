161b37892e16c24e36f613f75720e02b45625452
Bishal Thapaliya <bishalthapaliya@Bishals-MacBook-Pro.local>
2026-08-31 16:18:04 +0200

    feat: update environment variable handling

--- tests/envTest.spec.ts
+++ tests/envTest.spec.ts
@@ -1,7 +1,11 @@
 import { test } from '@playwright/test';
+import { ConfigReader } from "../utils/readConfigUtils";
 
 test('ENV Test', async ({ page }) => {
-    console.log(process.env.URL);
-    console.log(process.env.USERNAME);
-    console.log(process.env.PASSWORD);
+    // console.log(process.env.URL);
+    // console.log(process.env.USERNAME);
+    // console.log(process.env.PASSWORD);
+    console.log(ConfigReader.url);
+    console.log(ConfigReader.username);
+    console.log(ConfigReader.password);
 });
---
+++ utils/readConfigUtils.ts
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv';
+
+dotenv.config({ path: `./env/.env.${process.env.ENV}`});
+
+export class ConfigReader {
+
+    static readonly url = ConfigReader.getENV('URL');
+    static readonly username = ConfigReader.getENV('USERNAME');
+    static readonly password = ConfigReader.getENV('PASSWORD');
+
+    private static getENV(key: string): string {
+        const value = process.env[key];
+
+        if(!value) {
+            throw new Error(`Environment variable ${key} is not available`)
+        }
+        
+        return value
+    }
+}
