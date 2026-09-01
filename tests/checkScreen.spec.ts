314908dfb391b5d0ae3ce375dd8f5f12a344fe29
Bishal Thapaliya <bishalthapaliya@Bishals-MacBook-Pro.local>
2026-09-01 20:49:45 +0200

    test: add visual testing with toHaveScreenshot() to prevent UI layout shift

---
+++ tests/checkScreen.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+test('should check the screen', async ({ page }) => {
+    await page.goto('https://practicetestautomation.com/practice-test-login/');
+
+    await expect(page).toHaveScreenshot('fullPage.png', {fullPage: true});
+
+    await page.locator('#username').fill('student');
+    await page.locator('#password').fill('Password123');
+    await page.locator('#submit').click();
+    
+    await expect(page).toHaveScreenshot('LoginPage.png', { maxDiffPixelRatio: 0.02 });
+    await page.waitForTimeout(3000);
+});
