d98257cd6da8f1ceef1f3bebb3a257f1b046fc53
Bishal Thapaliya <bishalthapaliya@Bishals-MacBook-Pro.local>
2026-09-02 10:08:57 +0200

    refactor: remove full page screenshot check from login test

--- tests/checkScreen.spec.ts
+++ tests/checkScreen.spec.ts
@@ -3,8 +3,6 @@ import { test, expect } from '@playwright/test';
 test('should check the screen', async ({ page }) => {
     await page.goto('https://practicetestautomation.com/practice-test-login/');
 
-    await expect(page).toHaveScreenshot('fullPage.png', {fullPage: true});
-
     await page.locator('#username').fill('student');
     await page.locator('#password').fill('Password123');
     await page.locator('#submit').click();
