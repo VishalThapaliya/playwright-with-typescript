03d18f3a293ec2afac331da1885ad2ad8f0259da
Bishal Thapaliya <bishalthapaliya@Bishals-MacBook-Pro.local>
2026-09-02 15:43:54 +0200

    test: comment out full page screenshot check in checkScreen.spec.ts

--- tests/checkScreen.spec.ts
+++ tests/checkScreen.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 test('should check the screen', async ({ page }) => {
     await page.goto('https://practicetestautomation.com/practice-test-login/');
 
-    await expect(page).toHaveScreenshot('fullPage.png', { fullPage: true });
+    // await expect(page).toHaveScreenshot('fullPage.png', { fullPage: true });
 
     await page.locator('#username').fill('student');
     await page.locator('#password').fill('Password123');
